a6c69eb30f30b5eaafbb1b00e523d28f656ae17d
Manoj Kumar <kirodiwalmanoj@hotmail.com>
2017-07-30 21:06:23 +0530

    Added new Project

---
+++ Education.WebApp/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Education.Core;
+
+namespace Education.WebApp.Controllers
+{
+    public class UserController : Controller
+    {
+        public IUserReposetory _UserReporsetory;
+
+        public UserController()
+        {
+           // _UserReporsetory = _repo;
+        }
+        public UserController(IUserReposetory _repo)
+        {
+            _UserReporsetory = _repo;
+        }
+
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+       
+        [HttpPost]
+        public ActionResult Insert()
+        {
+            _UserReporsetory.InsertUserType("admin");
+            return View();
+
+        }
+    }
+}

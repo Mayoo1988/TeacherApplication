2d814ca527d4c1cd49736c86c6acc5508bfc94e6
Manoj Kumar <kirodiwalmanoj@hotmail.com>
2017-08-19 14:40:47 +0530

    changes done in unityconfig

--- Education.WebApp/App_Start/UnityConfig.cs
+++ Education.WebApp/App_Start/UnityConfig.cs
@@ -3,6 +3,8 @@ using Microsoft.Practices.Unity;
 using Unity.Mvc5;
 using Education.Core;
 using Education.WebApp.Controllers;
+using Education.Core.Students;
+using Education.Core.Admin;
 
 namespace Education.WebApp
 {
@@ -17,6 +19,15 @@ namespace Education.WebApp
 
             // e.g. container.RegisterType<ITestService, TestService>();
             container.RegisterType<IUserReposetory,UserReposetory>();
+            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+
+            container.RegisterType<IStudentReposetory, StudentReposetory>();
+            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+
+            container.RegisterType<IVideoUpload, VideoUpload>();
+            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+
+            container.RegisterType<ITestDetailsRepository, TestDetailsRepository>();
             DependencyResolver.SetResolver(new UnityDependencyResolver(container));
         }
     }
